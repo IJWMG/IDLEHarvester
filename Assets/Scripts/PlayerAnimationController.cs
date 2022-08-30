8c24d7caef8c977e0778c40c9d39d2742ab9ac40
Vladislav Shalkovskiy <vlad48516@mail.com>
2022-08-30 16:27:47 +0300

    animation commit

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private Avatar idleAvatar, runAvatar, cutAvatar;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate() {
+        Animate();
+       /* if (animator.GetBool("isCutting")){
+            animator.avatar = cutAvatar;
+        }
+        */
+    }
+    private void Animate(){
+        if (CustomInputSystem.GetInput() != Vector3.zero){
+            animator.SetBool("isRunning", true);
+        
+            //animator.avatar = runAvatar;
+          
+        }
+        else {
+            animator.SetBool("isRunning", false);
+            if(!animator.GetBool("isCutting")){
+                //animator.avatar = idleAvatar;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,33 @@ public class PlayerController : MonoBehaviour
 {
     private static float runSpeed = 0.1f;
     public static float RunSpeed { get { return runSpeed; } private set { runSpeed = RunSpeed; } }
-
+    private Animator animator;
+    [SerializeField] private ScytheScript scythePrefab;
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
     private void FixedUpdate()
     {
-       transform.Translate(CustomInputSystem.GetInput());
+        transform.Translate(CustomInputSystem.GetInput());
+        if (Input.GetKeyDown(KeyCode.E)){
+            animator.SetBool("isCutting", true);
+        }
     }
+    private void OnTriggerEnter(Collider other) {
+       /* Instantiate(
+            scythePrefab, 
+            new Vector3(
+                scythePrefab.transform.position.x, 
+                scythePrefab.transform.position.y, 
+                scythePrefab.transform.position.z
+                ), 
+            Quaternion.identity, 
+            GameObject.FindGameObjectWithTag("RightHand").transform
+        );
+        */
+        //animator.SetBool("isCutting", true);
+       
+    }
+    
+    
 }
